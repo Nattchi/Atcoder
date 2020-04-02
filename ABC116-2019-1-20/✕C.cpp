619061d931e3d44a3e6e1ec8937e6a2943659555
Nattchi <Yuu.tan@icloud.com>
2020-04-02 10:34:44 +0900

    ABC116 追加

---
+++ ABC116-2019-1-20/✕C.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> flowers(n);
+    int ans = 0;
+    int active = 0;
+    for (int i = 0; i < n; i++){
+        cin >> flowers.at(i);
+    }
+
+    for (int i = 0; i < n; i++){
+        if(active >= flowers.at(i)){
+            active = flowers.at(i);
+        }else{
+            ans += flowers.at(i) - active;
+            active = flowers.at(i);
+        }
+    }
+}
---
+++ ABC116-2019-1-20/〇B.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int f(int n){
+    if(n % 2 == 0){
+        return n / 2;
+    }else{
+        return n * 3 + 1;
+    }
+}
+
+int findIndex(int value , vector<int> vec){
+    vector<int>::iterator iter = std::find( vec.begin(), vec.end(), value);
+    int index = std::distance( vec.begin(), iter );
+    if(index == vec.size()){
+        return -1;
+    }
+    return index;
+}
+
+int main(){
+    int s;
+    cin >> s;
+
+    vector<int> nums(1000000);
+    int index = 0;
+    int insert = 0;
+    nums.at(index) = s;
+    bool flag = true;
+    while(flag){
+        ++index;
+        insert = f(nums.at(index - 1));
+        if(findIndex(insert, nums) != -1){
+            flag = false;
+        }else{
+            nums.at(index) = insert;
+        }
+    }
+    cout << index + 1 << endl;
+}
