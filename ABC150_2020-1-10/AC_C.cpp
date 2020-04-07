1525d89be2dda0170b7746cefe809a8554286975
Nattchi <Yuu.tan@icloud.com>
2020-04-07 19:58:59 +0900

    ABC150 追加

---
+++ ABC150_2020-1-10/AC_C.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <numeric>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int judge(vector<int> v, string P, string Q, int N){
+    int count = 1;
+    int a, b;
+    do{
+        string num;
+        for (int i = 0; i < N; i++){
+            num += to_string(v[i]);
+        }
+        if(num == P){
+            a = count;
+        }
+        if(num == Q){
+            b = count;
+        }
+        count++;
+    } while (next_permutation(v.begin(), v.end()));
+    return abs(a - b);
+}
+
+int main(){
+    int N;
+    cin >> N;
+    string P, Q, s;
+    vector<int> v(N);
+    iota(v.begin(), v.end(), 1);
+    for (int i = 0; i < N; i++){
+        cin >> s;
+        P += s;
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> s;
+        Q += s;
+    }
+    cout << judge(v, P, Q, N) << endl;
+}
