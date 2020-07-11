9017ad4407b1e8e4c55459bc5ea4dc7fe46b1d4a
Nattchi <Yuu.tan@icloud.com>
2020-07-11 23:01:25 +0900

    エイシング 追加

---
+++ エイシング_2020-7-11/AC_B.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    vector<int> a(N+1);
+    int ans = 0;
+    for (int i = 1; i <= N; i++){
+        cin >> a[i];
+        if(i % 2 != 0){
+            if(a[i] % 2 != 0) ans++;
+        }
+    }
+    cout << ans << endl;
+}
---
+++ エイシング_2020-7-11/AC_C.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    vector<int> ans(N + 1);
+    int n = sqrt(N);
+    for (int x = 1; x < n; x++){
+        for (int y = 1; y < n; y++){
+            for (int z = 1; z < n; z++){
+                int a = (x + y + z)*(x + y + z) - (x*y + y*z + z*x);
+                if(a >= 1 && a <= N) ans[a]++;
+            }
+        }
+    }
+
+    for (int i = 1; i <= N; i++){
+        cout << ans[i] << endl;
+    }
+}
