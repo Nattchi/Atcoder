44370c13b8614662653e48561cf756151e951163
Nattchi <Yuu.tan@icloud.com>
2020-10-05 23:58:12 +0900

    ABC172 追加

---
+++ ABC172-2020-6-27/AC_B.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string S, T;
+    cin >> S >> T;
+
+    int length = S.length();
+    int differ = 0;
+    for (int i = 0; i < length; i++){
+        if(S[i] != T[i]) differ++;
+    }
+    cout << differ << endl;
+}
