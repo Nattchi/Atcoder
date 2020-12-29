d35beabb0e7c510a3f77d83ff5401bcd4501080b
Nattchi <Yuu.tan@icloud.com>
2020-12-28 22:28:36 -0600

    NOMURA 追加

---
+++ NOMURA_2020-5-30/AC_A.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    int H1, M1, H2, M2, K;
+    cin >> H1 >> M1 >> H2 >> M2 >> K;
+
+    int wakeup_time = 0;
+
+    if(M1 >= M2){
+        H2 -= 1;
+        wakeup_time += (M2 + 60) - M1;
+    }else{
+        wakeup_time += M2 - M1;
+    }
+    wakeup_time += (H2 - H1) * 60;
+
+    if(wakeup_time >= K){
+        cout << wakeup_time - K << endl;
+    }else{
+        cout << 0 << endl;
+    }
+}
---
+++ NOMURA_2020-5-30/AC_B.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+char next_char(int i, string T){
+    return T[i + 1];
+}
+
+string replace(int i, string T){
+    switch(T[i]){
+        case 'P':
+            if(next_char(i, T) == '?'){
+                T[i + 1] = 'D';
+            }
+            break;
+        case 'D':
+            if(next_char(i, T) == '?'){
+                T[i + 1] = 'D';
+            }
+            break;
+        case '?':
+
+            break;
+    }
+}
+
+int main() {
+    string T;
+    cin >> T;
+
+    for (int i = 0; i < T.length(); i++){
+        if(T[i] == '?'){
+            T[i] = 'D';
+        }
+    }
+
+    cout << T << endl;
+}
